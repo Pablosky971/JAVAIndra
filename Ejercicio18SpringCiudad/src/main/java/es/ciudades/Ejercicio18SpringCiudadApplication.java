0e188ff337b7c69a1da389a0f0e5443f0762f1bd
Pablosky971 <pablosky971@gmail.com>
2023-05-18 13:34:36 +0200

    Finalizado ejercicio 18

---
+++ Ejercicio18SpringCiudad/src/main/java/es/ciudades/Ejercicio18SpringCiudadApplication.java
@@ -0,0 +1,13 @@
+package es.ciudades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio18SpringCiudadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio18SpringCiudadApplication.class, args);
+	}
+
+}
---
+++ Ejercicio18SpringCiudad/src/test/java/es/ciudades/Ejercicio18SpringCiudadApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ciudades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio18SpringCiudadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
